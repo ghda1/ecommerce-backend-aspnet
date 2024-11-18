ffa14ff02a691599b2ee5d6317c559f5efa2b37d
ghadah <gh.gadahd01@gmail.com>
2024-11-18 21:00:54 +0300

    Try to update swagger

--- src/Program.cs
+++ src/Program.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using System.Text;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
@@ -9,25 +10,6 @@ using Npgsql;
 using UserAuthenticationWebApi2.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
-DotNetEnv.Env.Load();
-
-
-builder.Services.AddAutoMapper(typeof(Program));
-
-
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IAddressService, AddressService>();
-builder.Services.AddScoped<IPaymentService, PaymentService>();
-builder.Services.AddScoped<IUserAuthentication, UserAuthenticationService>();
-builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<ISizeService, SizeService>();
-builder.Services.AddScoped<IColorService, ColorService>();
-builder.Services.AddScoped<IOrderService, OrderService>();
-builder.Services.AddScoped<IShipmentService, ShipmentService>();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(options =>
     {
@@ -42,33 +24,42 @@ builder.Services.AddCors(options =>
         });
     });
 
-//Services Controller Configuration with JsonIgnore
-builder.Services.AddControllers()
-    .AddNewtonsoftJson(options =>
-    {
-        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-        options.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.None;
-        options.SerializerSettings.MetadataPropertyHandling = Newtonsoft.Json.MetadataPropertyHandling.Ignore;
-        options.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter { CamelCaseText = true });
-    });
 
-var Configuration = builder.Configuration;
+DotNetEnv.Env.Load();
 
 // Get JWT settings from environment variables
 var jwtKey = Environment.GetEnvironmentVariable("Jwt__KEY") ?? throw new InvalidOperationException("JWT Key is missing in environment variables.");
 System.Console.WriteLine($"JwtKey {jwtKey}");
 var jwtIssuer = Environment.GetEnvironmentVariable("Jwt__ISSUER") ?? throw new InvalidOperationException("JWT Issuer is missing in environment variables.");
 var jwtAudience = Environment.GetEnvironmentVariable("Jwt__AUDIENCE") ?? throw new InvalidOperationException("JWT Audience is missing in environment variables.");
+
 var defaultConnection = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection") ?? throw new InvalidOperationException("Default Connection is missing in environment variables.");
 
 builder.Services.AddDbContext<AppDBContext>(options =>
   options.UseNpgsql(defaultConnection));
+
+
+var Configuration = builder.Configuration;
+
 
 var dataSourceBuilder = new NpgsqlDataSourceBuilder(Configuration.GetConnectionString("Local"));
 dataSourceBuilder.MapEnum<PaymentMethod>();
 dataSourceBuilder.MapEnum<Material>();
 dataSourceBuilder.MapEnum<Status>();
 
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IAddressService, AddressService>();
+builder.Services.AddScoped<IPaymentService, PaymentService>();
+builder.Services.AddScoped<IUserAuthentication, UserAuthenticationService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ISizeService, SizeService>();
+builder.Services.AddScoped<IColorService, ColorService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IShipmentService, ShipmentService>();
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddEndpointsApiExplorer();
 
 var key = Encoding.ASCII.GetBytes(jwtKey);
 builder.Services.AddAuthentication(options =>
@@ -92,10 +83,22 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Services Controller Configuration with JsonIgnore
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+        options.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.None;
+        options.SerializerSettings.MetadataPropertyHandling = Newtonsoft.Json.MetadataPropertyHandling.Ignore;
+        options.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter { CamelCaseText = true });
+    });
+
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-    // Add JWT Authentication to Swagger
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         In = ParameterLocation.Header,
@@ -125,13 +128,17 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.MapGet("", (HttpRequest request) =>
+{
+    return Results.Ok("Welcome to TeeNest Api");
+
+});
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseHttpsRedirection();
 
 app.Use(async (context, next) =>
 {
@@ -148,7 +155,7 @@ app.Use(async (context, next) =>
 
 
 app.UseCors("MyAllowSpecificOrigins");
-
+app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
